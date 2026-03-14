2f3a0fb4c125ab3db52384446ffc360b0b8070e7
Dwiriz12 <dwirizkiindriyanti12@gmail.com>
2026-03-14 18:24:13 +0700

    second commit

--- TP_MODUL4_103022430013/Program.cs
+++ TP_MODUL4_103022430013/Program.cs
@@ -1,1 +1,46 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+public class KodePos
+{
+    string[] kelurahan =
+    {
+        "Batununggal","Kujangsari","Mengger","Wates","Cijaura",
+        "Jatisari","Margasari","Sekejati","Kebonwaru","Maleer"
+    };
+
+    int[] kodePos =
+    {
+        40266,40287,40267,40256,40287,
+        40286,40286,40286,40272,40274
+    };
+
+    public int getKodePos (string namaKelurahan)
+    {
+        for(int i = 0; i < kelurahan.Length; i++)
+        {
+            if (kelurahan[i] == namaKelurahan)
+            {
+                return kodePos[i];
+            }
+        }
+        return -1;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        KodePos kode = new KodePos();
+
+        Console.WriteLine("Batununggal : " + kode.getKodePos("Batununggal"));
+        Console.WriteLine("Kujangsari  : " + kode.getKodePos("Kujangsari"));
+        Console.WriteLine("Mengger     : " + kode.getKodePos("Mengger"));
+        Console.WriteLine("Wates       : " + kode.getKodePos("Wates"));
+        Console.WriteLine("Cijaura     : " + kode.getKodePos("Cijaura"));
+        Console.WriteLine("Jatisari    : " + kode.getKodePos("Jatisari"));
+        Console.WriteLine("Margasari   : " + kode.getKodePos("Margasari"));
+        Console.WriteLine("Sekejati    : " + kode.getKodePos("Sekejati"));
+        Console.WriteLine("Kebonwaru   : " + kode.getKodePos("Kebonwaru"));
+        Console.WriteLine("Maleer      : " + kode.getKodePos("Maleer"));
+    }
+}
